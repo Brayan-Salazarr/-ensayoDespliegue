81b00584807717ccd2f2da80b47d9f658c72014e
Brayan-Salazarr <brayansalazzar@gmail.com>
2025-11-25 13:10:13 -0500

    route

---
+++ src/main/java/com/example/adso/controller/routeController.java
@@ -0,0 +1,13 @@
+package com.example.adso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class routeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "API funcionando correctamente";
+    }
+}
