b9437f87966aa6a28fc7ad78c83611161d4a0096
Brayan-Salazarr <brayansalazzar@gmail.com>
2025-11-25 14:07:25 -0500

    as

--- src/main/java/com/example/adso/config/SecurityConfig.java
+++ src/main/java/com/example/adso/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
            public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
 
-                configuration.setAllowedOrigins(Arrays.asList("https://front-ensayo.vercel.app/login"));
+                configuration.setAllowedOrigins(Arrays.asList("https://front-ensayo.vercel.app"));
                 configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 configuration.setAllowedHeaders(Arrays.asList("*"));
                 configuration.setAllowCredentials(true);
